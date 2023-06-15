0f92207c956942dbf2d3d934ba3087add4483cd0
rendani99 <rendania460@gmail.com>
2023-06-15 19:13:34 +0200

    exploring StyleSheet import

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>I love CODING!</Text>
+      <View>
+        <Text>I love coding in js </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     display: 'flex',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 })
